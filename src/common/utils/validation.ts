d9d38fb09c80a7a332c3de016d47d7ffee2fc2b3
JEWONCHOI <alsrn6040@naver.com>
2025-04-12 13:59:07 +0900

    [init] databsae env value validation, typeorm package install

--- src/common/utils/validation.ts
+++ src/common/utils/validation.ts
@@ -3,6 +3,10 @@ import * as Joi from 'joi';
 export const validation: Joi.Schema = Joi.object({
   NODE_ENV: Joi.string().valid('development', 'production').required(),
   SERVER_PORT: Joi.number().required(),
+  DB_HOST: Joi.number().required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_NAME: Joi.string().required(),
 }).options({
   abortEarly: true,
 });
