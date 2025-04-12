e16bb2160dbccb05447d29c63cf7d95028f73ae2
JEWONCHOI <alsrn6040@naver.com>
2025-04-12 17:02:04 +0900

    [feat] auth guard

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { TokenModule } from 'src/token/token.module';
 
 @Module({
+  imports: [TokenModule],
   controllers: [UserController],
+  providers: [],
 })
 export class UserModule {}
