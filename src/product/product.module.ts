e5ee774718a3125931fff723e22ce1ede49a9f45
JEWONCHOI <alsrn6040@naver.com>
2025-04-12 18:11:15 +0900

    [feat] product entity, provider

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { DatabaseModule } from 'src/database/database.module';
+import { productProviders } from './providers/product.provider';
 
 @Module({
-  providers: [ProductService]
+  imports: [DatabaseModule],
+  providers: [...productProviders, ProductService],
+  exports: [ProductService],
 })
 export class ProductModule {}
