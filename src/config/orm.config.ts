c1105024d5cd514bd93f836bd7f7028fd072d02b
JEWONCHOI <alsrn6040@naver.com>
2025-04-12 13:59:48 +0900

    [init] orm cli

---
+++ src/config/orm.config.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+dotenv.config({
+  path: path.resolve(__dirname, `../../.${NODE_ENV}.env`),
+});
+
+export default new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: +(process.env.DB_PORT as string),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: [path.resolve(__dirname, '../entities/**/*.entity.{ts,js}')],
+  migrations: [path.resolve(__dirname, '../migrations/*.{ts,js}')],
+  synchronize: false,
+});
