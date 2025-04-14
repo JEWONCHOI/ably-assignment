8706b1a02b70fa51d87350862d4d8ef2fd1b6cef
CHOIJEWON <alsrn6040@kakao.com>
2025-04-14 20:15:08 +0900

    [refacor] 사용하지 않는 유틸 함수 dto 삭제

--- src/common/dto/pagination.dto.ts
+++ src/common/dto/pagination.dto.ts
@@ -1,31 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class ChangePaginationFormDto<T> {
-  @ApiProperty({ description: '데이터 리스트' })
-  data: T;
-  @ApiProperty({ description: '전체 데이터 개수' })
-  totalElement: number;
-  @ApiProperty({ description: '데이터 take 개수' })
-  take: number;
-  @ApiProperty({ description: '페이지네이션 현재 페이지' })
-  page: number;
-  @ApiProperty({ description: '데이터 key name' })
-  dataName: string;
-}
-
-export class ChangePaginationFormResponse<T> {
-  @ApiProperty({ description: '전체 데이터 개수' })
-  totalElement: number;
-
-  @ApiProperty({ description: '전체 페이지 수' })
-  totalPages: number;
-
-  @ApiProperty({ description: '현재 조회중인 페이지' })
-  currentPage: number;
-
-  [key: string]: T | number;
-}
-
 const PAGINATION_EXAMPLE_DATA = [
   {
     id: 284,
--- src/common/utils/pagiantaion-from.ts
+++ src/common/utils/pagiantaion-from.ts
@@ -1,20 +1,7 @@
 import {
   ChangeCursorPagiFormDto,
   ChangeCursorPagiFormResponse,
-  ChangePaginationFormDto,
-  ChangePaginationFormResponse,
 } from '../dto/pagination.dto';
-
-export function changePaginationForm<T>(
-  dto: ChangePaginationFormDto<T>,
-): ChangePaginationFormResponse<T> {
-  return {
-    [dto.dataName]: dto.data,
-    totalElement: dto.totalElement,
-    totalPages: Math.ceil(dto.totalElement / dto.take),
-    currentPage: dto.page,
-  };
-}
 
 export function changeCursorPagiForm<T extends { id: number }>(
   changeCursorPagiFormDto: ChangeCursorPagiFormDto<T>,
